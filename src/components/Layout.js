5f95d1ded7f7ccbf36664451c8e7c1d25fff4b61
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2022-03-13 14:19:57 +0400

    feat: click outside to close menu

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,41 +1,55 @@
-import React, {useState, useEffect} from "react";
+import React, {useEffect, useRef, useState} from "react";
+import {Link} from "react-router-dom";
 
 const Layout = (props) => {
     const [open, setOpen] = useState(false);
+    const dropDownRef = useRef();
 
-    // useEffect(() => {
-    //
-    //     document.addEventListener('mousedown', () => {
-    //         setOpen(false);
-    //     })
-    // }, [])
+    useEffect(() => {
+        /*
+        * close language
+        * menu if you click
+        * outside a target
+        * */
+        let handler = event => {
+            if (!dropDownRef.current.contains(event.target) || event.target.classList.contains('lang')) {
+                setOpen(false);
+            }
+        }
+
+        document.addEventListener('click', handler);
+
+        return () => {
+            document.removeEventListener('click', handler);
+        };
+    })
 
     return (
         <React.Fragment>
-            <div className="absolute right-1/2 top-2">
+            <div ref={dropDownRef} className="absolute right-1/2 top-2">
                 <ul>
                     <li>
-                        <a onClick={() => setOpen(!open)} className="flex items-center cursor-pointer">
+                        <button onClick={() => setOpen(!open)} className="flex items-center cursor-pointer">
                             English
                             <span>
                                 <img className="ml-2" src={require('../assets/img/Stroke 165.png')} alt="arrowDown"/>
                             </span>
-                        </a>
+                        </button>
                         {open && <div
                             className="shadow-md w-36 absolute left-0 top-9 rounded-lg bg-gray-200 bg-opacity-75 text-black">
                             <ul>
                                 <li>
-                                    <a href='#en'
-                                       className="text-left px-4 p-3 transition duration-150 ease-in hover:bg-gray-300 rounded w-full block">
+                                    <Link to='#en'
+                                          className="lang text-left px-4 p-3 transition duration-150 ease-in hover:bg-gray-300 rounded w-full block">
                                         English
-                                    </a>
+                                    </Link>
                                 </li>
 
                                 <li>
-                                    <a href="#ka"
-                                       className="text-left px-4 p-3 transition duration-150 ease-in hover:bg-gray-300 rounded w-full block">
+                                    <Link to="#ka"
+                                          className="lang text-left px-4 p-3 transition duration-150 ease-in hover:bg-gray-300 rounded w-full block">
                                         ქართული
-                                    </a>
+                                    </Link>
                                 </li>
                             </ul>
                         </div>}
